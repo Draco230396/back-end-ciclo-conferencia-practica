c28ffee0bd1ca8ad8cbaea19bc94cfc4a89e63f9
Romeo Cuahua Galvez <romeo.cuahua@neoris.com>
2022-12-07 23:57:55 -0600

    Subiendo las carpetas de test

---
+++ src/test/java/com/axl/practica/StudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axl.practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
